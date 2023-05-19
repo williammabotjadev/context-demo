1541bada378102e4ac98a7194e52730295664525
williammabotjadev <williammabotjadev@gmail.com>
2023-05-19 17:48:09 +0200

    UserContext demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { User } from './User';
+import { userContext } from './Context';
 
 function App() {
+
+  const userObj = {
+    name: "William",
+    surname: "Mabotja",
+    country: "South Africa"
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <userContext.Provider value={userObj}>
+          <User />
+      </userContext.Provider>
+    </>
   );
 }
 
